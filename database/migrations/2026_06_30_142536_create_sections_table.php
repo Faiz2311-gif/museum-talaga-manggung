5dc15952fcc2a4c51e200cd23c21f5892c8fedd0
Faiz2311-gif <safaraztf@gmail.com>
2026-07-02 09:24:51 +0700

    95% complete

--- database/migrations/2026_06_30_142536_create_sections_table.php
+++ database/migrations/2026_06_30_142536_create_sections_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('sections', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('sections', function (Blueprint $table) {
+        $table->id();
+        $table->string('page');    // Untuk memisahkan konten (contoh: 'sejarah', 'beranda')
+        $table->string('key');     // Kata kunci field teks (contoh: 'sejarah_title')
+        $table->text('content');   // Isi narasi artikel/tulisan
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
